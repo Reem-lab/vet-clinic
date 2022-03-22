604f5650250997a907335cc000358bbf0e700cfc
Reem <janinareem@gmail.com>
2022-03-22 18:08:05 +0200

    insert the data into the table

--- data.sql
+++ data.sql
@@ -1,5 +1,16 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+
+
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered, weight_kg)
+VALUES ('Agumon', 'Feb 3rd, 2020', 0 , true, 10.23);
+
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered, weight_kg)
+VALUES ('Gabumon', 'Nov 15th, 2018', 2 , true, 8);
+
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered, weight_kg)
+VALUES ('Pikachu', 'Jan 7th, 2021', 1 , false, 15.04);
+
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered, weight_kg)
+VALUES ('Devimon', 'May 12th, 2017', 5 , true, 11);
+
