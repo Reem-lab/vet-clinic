801ca6280f4c92116ce6275dc627c29bb20acfd8
Reem <janinareem@gmail.com>
2022-03-23 01:25:45 +0200

    insert more animals information

--- data.sql
+++ data.sql
@@ -1,16 +1,15 @@
 /* Populate database with sample data. */
 
 
-
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered, weight_kg)
-VALUES ('Agumon', 'Feb 3rd, 2020', 0 , true, 10.23);
-
-INSERT INTO animals (name,date_of_birth,escape_attempts,neutered, weight_kg)
-VALUES ('Gabumon', 'Nov 15th, 2018', 2 , true, 8);
-
-INSERT INTO animals (name,date_of_birth,escape_attempts,neutered, weight_kg)
-VALUES ('Pikachu', 'Jan 7th, 2021', 1 , false, 15.04);
-
-INSERT INTO animals (name,date_of_birth,escape_attempts,neutered, weight_kg)
-VALUES ('Devimon', 'May 12th, 2017', 5 , true, 11);
-
+VALUES ('Agumon', 'Feb 3rd, 2020', 0 , true, 10.23),
+       ('Gabumon', 'Nov 15th, 2018', 2 , true, 8),
+       ('Pikachu', 'Jan 7th, 2021', 1 , false, 15.04),
+       ('Devimon', 'May 12th, 2017', 5 , true, 11),
+	   ('Charmander', 'Feb 8th, 2020', 0 , false, -11),
+	   ('Plantmon', ' Nov 15th, 2021', 2 , true, -5.7),
+	   ('Squirtle', ' Apr 2nd, 1993', 3 , false, -12.13),
+	   ('Angemon', 'Jun 12th, 2005', 1 , true, -45),
+	   ('Boarmon', 'Jun 7th, 2005', 7 , true, 20.4),
+	   ('Blossom', 'Oct 13th, 1998', 3 , true, 17),
+	   ('Ditto', 'May 14th, 2022', 4 , true, 22);
