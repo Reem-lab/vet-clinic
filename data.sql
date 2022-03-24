f9e4265276cb7c4cd6987ff98c4163cc5165b40e
Reem <janinareem@gmail.com>
2022-03-24 02:26:39 +0200

    Insert the information for the owners and specices tables

--- data.sql
+++ data.sql
@@ -2,14 +2,28 @@
 
 
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered, weight_kg)
-VALUES ('Agumon', 'Feb 3rd, 2020', 0 , true, 10.23),
-       ('Gabumon', 'Nov 15th, 2018', 2 , true, 8),
-       ('Pikachu', 'Jan 7th, 2021', 1 , false, 15.04),
-       ('Devimon', 'May 12th, 2017', 5 , true, 11),
-	   ('Charmander', 'Feb 8th, 2020', 0 , false, -11),
-	   ('Plantmon', ' Nov 15th, 2021', 2 , true, -5.7),
-	   ('Squirtle', ' Apr 2nd, 1993', 3 , false, -12.13),
-	   ('Angemon', 'Jun 12th, 2005', 1 , true, -45),
-	   ('Boarmon', 'Jun 7th, 2005', 7 , true, 20.4),
-	   ('Blossom', 'Oct 13th, 1998', 3 , true, 17),
-	   ('Ditto', 'May 14th, 2022', 4 , true, 22);
+VALUES ('Agumon', '3-2-2020', 0 , true, 10.23),
+       ('Gabumon', '11-15-2018', 2 , true, 8),
+       ('Pikachu', '1-7-2021', 1 , false, 15.04),
+       ('Devimon', '5-12-2017', 5 , true, 11),
+	   ('Charmander', '2-8-2020', 0 , false, -11),
+	   ('Plantmon', '11-15-2021', 2 , true, -5.7),
+	   ('Squirtle', '4-2-1993', 3 , false, -12.13),
+	   ('Angemon', '6-12-2005', 1 , true, -45),
+	   ('Boarmon', '6-7-2005', 7 , true, 20.4),
+	   ('Blossom', '10-13-1998', 3 , true, 17),
+	   ('Ditto', '5-14-2022', 4 , true, 22);
+
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+       ('Jennifer Orwell', 19),
+	   ('Bob', 45),
+	   ('Melody Pond', 77),
+	   ('Dean Winchester', 14),
+	   ('Jodie Whittaker ', 38);
+
+INSERT INTO species (name)
+VALUES ('pokemon'),
+       ('Digimon');
+
